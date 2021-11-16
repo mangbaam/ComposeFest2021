5d6c926363ebb7f0395098c41a30292a76d98970
mangbaam <pmb0836@gmail.com>
2021-11-17 02:35:17 +0900

    [week2] Complex custom layout

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/mangbaam/compose/week2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/mangbaam/compose/week2/MainActivity.kt
@@ -13,14 +13,21 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import kotlinx.coroutines.launch
@@ -32,9 +39,201 @@ class MainActivity : ComponentActivity() {
         setContent {
             Week2Theme {
                 // A surface container using the 'background' color from the theme
-                ScrollingList()
+                BodyContent3()
             }
         }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // 각 행의 너비 추적
+        val rowWidths = IntArray(rows) {0}
+        // 각 행의 최대 높이 추적
+        val rowHeights = IntArray(rows) {0}
+
+        // 측정된 자식 리스트
+        val placeable = measurables.mapIndexed { index, measurable ->
+            // 각 자식 측정
+            val placeable = measurable.measure(constraints)
+            // 각 행의 너비와 최대 높이 추적
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // 그리드의 너비는 가장 넓은 행
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        // 그리드의 높이는 각 행의 가장 큰 요소의 합
+        val height = rowHeights.sumOf {it}
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // 이전 행의 축적된 높이에 기반한 각 행의 Y
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        // 부모 레이아웃의 사이즈 설정
+        layout(width, height) {
+            // 행당 우리가 배치한 x 코드
+            val rowX = IntArray(rows) {0}
+
+            placeable.forEachIndexed { index, placeable ->
+                val row = index%rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier
+                .size(16.dp, 16.dp)
+                .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun BodyContent3(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier, rows = 5) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Week2Preview() {
+    Week2Theme {
+        BodyContent3()
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    Week2Theme {
+        Chip(text = "안녕 여러분~")
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier:Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurable, constraints ->
+        // Measure children
+        val placeable = measurable.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // 최대한의 크기로 설정
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place Children
+            placeable.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent2(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Preview
+@Composable
+fun MyOwnColumnPreview() {
+    BodyContent2()
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaseLinePreview() {
+    Week2Theme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Week2Theme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
@@ -114,7 +313,7 @@ fun ImageListItem(index: Int) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
 fun LazyListPreview() {
     LazyList()
@@ -148,7 +347,7 @@ fun BodyContent(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun LayoutCodelabPreview() {
     Week2Theme {
